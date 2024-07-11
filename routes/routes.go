869c3c02fe4c8d802e883caf4d7c0ed022327f15
Luan Diniz <luan.diniz23@hotmail.com>
2024-07-11 08:51:10 -0300

    finalizacao da documentacao da rota de buscar alunos

--- routes/routes.go
+++ routes/routes.go
@@ -4,10 +4,17 @@ import (
 	"gin-api-go/controllers"
 
 	"github.com/gin-gonic/gin"
+
+	docs "gin-api-go/docs"
+   swaggerfiles "github.com/swaggo/files"
+   ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func HandleRequests() {
 	r := gin.Default()
+	docs.SwaggerInfo.BasePath = "/"
+	
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	r.GET("/alunos", controllers.BuscarAlunos)
 	r.GET("/alunos/:id", controllers.BuscarAlunoById)
 	r.GET("/alunos/cpf/:cpf", controllers.BuscarAlunoByCpf)
